2b826b2cb1c6cbc6437830063f2e36a524dabba7
johsnow <johsnow08@gmail.com>
2017-04-21 09:55:49 +0800

    implement edit/update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,4 +7,44 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+
+  end
+
+
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+
+
 end
